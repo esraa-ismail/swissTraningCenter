995e455be778c3f84441cdf9dc27a7e426ed5af5
esraa magdy ismail <73431212+esraa-ismail@users.noreply.github.com>
2022-11-22 09:11:08 +0200

    adding my project

---
+++ JS/Main.js
@@ -0,0 +1,33 @@
+
+    $(document).ready(function(){
+        $('.your-class').slick({
+            autoplay: true,
+            dots: false,
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                }
+            ]
+        });
+      });
+      $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
