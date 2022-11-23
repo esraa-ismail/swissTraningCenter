28f158d6dcd1e65ba1ca51c9a9eede8f5fb6252f
esraa magdy ismail <73431212+esraa-ismail@users.noreply.github.com>
2022-11-23 12:25:14 +0200

    improve

--- JS/Main.js
+++ JS/Main.js
@@ -1,29 +1,35 @@
+const observer = new IntersectionObserver(entries => {
+entries.forEach(entry => {
+  if(entry.isIntersecting){
+    document.querySelectorAll(".animated")[0].classList.add("fadeintop");
+    document.querySelectorAll(".animated")[1].classList.add("fadeintop");
+    document.querySelectorAll(".animated")[2].classList.add("fadeintop");
+    document.querySelectorAll(".animated")[3].classList.add("fadeintop");
 
+
+  }
+}
+  )
+})
+
+observer.observe(document.querySelectorAll(".container-fluid"));
+    
+    
+    
+    
+    
+    
+    
+    
+    
     $('.autoplay').slick({
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1000,
-        arrows: false,
 
       });
 
 
 
-      $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-nav'
-      });
-      $('.slider-nav').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        dots: true,
-        centerMode: true,
-        focusOnSelect: true,
-        arrows: false,
-
-      });
+      
