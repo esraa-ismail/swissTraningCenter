322f1b863398dde0d90c19317952f98159c0e10e
esraa magdy ismail <73431212+esraa-ismail@users.noreply.github.com>
2022-11-24 07:33:02 +0200

    add counter

--- JS/Main.js
+++ JS/Main.js
@@ -1,21 +1,68 @@
-const observer = new IntersectionObserver(entries => {
-entries.forEach(entry => {
-  if(entry.isIntersecting){
-    document.querySelectorAll(".animated")[0].classList.add("fadeintop");
-    document.querySelectorAll(".animated")[1].classList.add("fadeintop");
-    document.querySelectorAll(".animated")[2].classList.add("fadeintop");
-    document.querySelectorAll(".animated")[3].classList.add("fadeintop");
 
 
+  
+// const section1 = document.querySelector(".whyOurCompany");
+
+// const animate = document.querySelector(".esraa")
+
+
+// if(window.scrollY >= section1.offsetTop){
+//    animate.classList.add("fadeinleft"); }
+
+  
+  // }
+
+// const observer = new IntersectionObserver(entries => {
+//       entries.forEach(entry => {
+//         document.querySelectorAll(".animate")[0].classList.add("fadeintop");
+
+//           console.log(entry);
+//       });
+//   });
+
+  
+
+
+    
+    
+  //count section
+  function count (){
+
+    
+  const nums = document.querySelectorAll(".nums .num");
+  let section = document.querySelector(".thelast");
+  let started = false ;
+  const element2 = document.querySelector(".count");
+
+
+  window.onscroll = function(){
+    //count section
+   if(window.scrollY >= section.offsetTop){
+     element2.classList.add("fadeintop");
+
+      if (!started) {
+        nums.forEach((num)=> startcount(num))
+
+      }
+      started = true ;
+    }
   }
-}
-  )
-})
 
-observer.observe(document.querySelectorAll(".container-fluid"));
-    
-    
-    
+  
+  function startcount(el){
+    let goal = el.dataset.goal ;
+    let count = setInterval(()=>{
+         el.textContent ++
+         if(el.textContent == goal) {
+          clearInterval(count)
+         }
+    },2000/goal)
+
+  } }
+  count();
+
+ 
+  
     
     
     
